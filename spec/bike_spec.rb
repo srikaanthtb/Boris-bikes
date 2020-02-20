cf82bcb260fe8efb57c65ca2025b384310543e8f
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-02-20 12:27:59 +0000

    Updated step 6 to step 10

---
+++ spec/bike_spec.rb
@@ -0,0 +1,6 @@
+require_relative '../lib/bike'
+describe Bike do
+   context "is bike working?" do
+       it {is_expected.to respond_to :working?}
+   end
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,13 @@
-require_relative '../dockingstation'
+require_relative '../lib/dockingstation'
 describe DockingStation do
+    context "releases bike" do
+        it {is_expected.to respond_to :release_bike}
+    end
+end
 
+describe DockingStation do
+    it 'is expected to return true' do
+        Bike = Bike.new
+        expect(Bike.working?).to be true
+    end
 end

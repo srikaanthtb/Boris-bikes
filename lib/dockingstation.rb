8b363720e571e50663d156e7ec110fce548639e9
srikaanthtb <srikaanth.t.b@gmail.com>
2020-02-21 16:57:30 +0000

    step 15 done NOW

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -5,11 +5,19 @@ class DockingStation
   @bikes = []
   end
   def release_bike
-      fail "No bikes available" if @bikes.empty?
+      fail "No bikes available" if empty?
       @bikes.pop
   end
   def dock(bike)
-    fail 'Docking station full' if @bikes.count >= 20
+    fail 'Docking station full' if full
     @bikes << bike
+  end
+
+def full
+  @bikes.count >= 20
+end
+
+  def empty?
+    @bikes.empty?
   end
 end
